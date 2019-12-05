c63db97b1ab6fea081e39a1e1a9d6431f870832f
Vishal Dubey <Vishal.Dubey@lntinfotech.com>
2019-12-06 03:33:43 +0530

    vishal commit

---
+++ src/main/resources/com/lti/login/LoginServlet.java
@@ -0,0 +1,46 @@
+package com.lti.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String username=request.getParameter("firstname");
+		String password=request.getParameter("lastname");
+		if(username.equals(password))
+			request.getRequestDispatcher("Success.jsp").forward(request, response);
+		else
+			request.getRequestDispatcher("Failure.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
